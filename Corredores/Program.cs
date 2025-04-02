7853bd52b9b894070f14e290fef33575e9c90eff
Karenn <karenggarcia00@gmail.com>
2025-04-02 13:06:44 -0600

    Agregación de  corredor C

--- Corredores/Program.cs
+++ Corredores/Program.cs
@@ -13,12 +13,16 @@ class Program
         // Crear dos corredores
         Thread corredorA = new Thread(Correr);
         Thread corredorB = new Thread(Correr);
+        Thread corredorC = new Thread(Correr);
 
         corredorA.Start("Corredor A");
         corredorB.Start("Corredor B");
+        corredorC.Start("Corredor C");
+
 
         corredorA.Join();
         corredorB.Join();
+        corredorC.Join();
 
         Console.WriteLine("¡Carrera terminada!");
     }
